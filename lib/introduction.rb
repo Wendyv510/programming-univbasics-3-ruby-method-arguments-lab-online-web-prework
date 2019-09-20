cc545c4265a11a26809209e439d5f884b033f1ab
Wendy Harris <wendyv510@hotmail.com>
2019-09-20 18:34:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,8 +2,8 @@ def introduction_with_language = (name and language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end 
 
-name = "Dan"
-language = "Ember.js"
+name = "Josh"
+language = "React"
 
 
 
