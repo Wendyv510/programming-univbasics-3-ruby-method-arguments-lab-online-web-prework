e7e0f36aa0c860f244a76af42e5c57d005bd978c
Wendy Harris <wendyv510@hotmail.com>
2019-09-20 18:28:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,15 @@
+def introduction = (name)
+  puts "Hi, my name is #{name}"
+end 
+
+name = "Josh"
+
+
+
+
+
+
+
 describe "introduction" do
   it "takes in an argument of a name and puts out a phrase with that name using string interpolation"do
     expect {introduction ("Josh")}. to output ("Hi, my name is Josh.\n").to_stdout
@@ -12,7 +24,11 @@ describe "#introduction_with_language"do
   end
 end
 
-describe 
+describe "introduction_with_language_optional"do
+  it "takes in two arguments, a name and a language, and language defualts to Ruby"do
+    expect (introduction_with_language_optional ("Edwin")).to output ("Hi, my name is Edwin and I am learning to program in Ruby.\n").to_stdout
+  end
+end 
 
 
 
